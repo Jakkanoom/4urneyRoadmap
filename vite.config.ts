44ed5930c59b2b66342d291f68f194863913b8ef
JakapanN <jakapan@4plus.co.th>
2024-12-13 02:09:06 +0700

    'deploy'

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
   base: '/4urneyRoadmap/', // Replace <repository> with your GitHub repo name
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },  
 });
