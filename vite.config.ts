a07bb35ddb3146d05b0b12813f686b4dec4fe94a
JakapanN <jakapan@4plus.co.th>
2024-12-17 14:38:47 +0700

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,18 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import path from 'path';
 
 export default defineConfig(({ command }) => {
   return {
     plugins: [vue()],
-    base: command === 'build' ? '/4urneyRoadmap/' : '/', // Set base dynamically
+    base: '/4urneyRoadmap/',
     resolve: {
       alias: {
-        '@': resolve(__dirname, './src'),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     build: {
-      outDir: 'dist',
+      outDir: 'dist', // Output to 'dist' folder
     },
   };
 });
