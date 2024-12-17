ffd905fa33f8caa2eb6b72fcf95a34eb02f22e7f
JakapanN <jakapan@4plus.co.th>
2024-12-17 15:10:45 +0700

    Fix vite.config.ts for base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/4urneyRoadmap/', // Correct base path
+  base: '/4urneyRoadmap/', // Use for production (GitHub Pages)
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
