b2c79fe9f58356283051666b4a23a49850c5fb3b
JakapanN <jakapan@4plus.co.th>
2024-12-17 14:20:35 +0700

    Update Package

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,22 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
-export default defineConfig({
-  plugins: [vue()],
-  base: '/4urneyRoadmap/', // Use this for GitHub Pages
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets', // Ensures assets are placed in "assets" directory
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [vue()],
+    base: command === 'build' ? '/4urneyRoadmap/' : '/', // Set base dynamically
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
       },
     },
-  },
+    build: {
+      outDir: 'dist',
+    },
+  };
 });
-
