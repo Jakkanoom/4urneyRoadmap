ac875545df058a749976caf29dd98f0f1c6ffbd6
JakapanN <jakapan@4plus.co.th>
2024-12-17 13:35:14 +0700

    'deploy'

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,22 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/4urneyRoadmap/', // Replace <repository> with your GitHub repo name
+  base: '/4urneyRoadmap/', // Use this for GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },  
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets', // Ensures assets are placed in "assets" directory
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
 });
+
